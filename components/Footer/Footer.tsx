0c97e4f90f3527a773c8c01bf9c9452881b65759
Yanger Rai <v.bikashrai@gmail.com>
2024-07-03 00:27:14 +0530

    updated the footer with our detail

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,9 +3,10 @@ import * as React from 'react';
 const Footer = () => {
   return (
     <footer className="w-full py-4 bg-gray-800 text-white text-center">
-      <p>
-        © {new Date().getFullYear()} C2 Tech. All rights reserved. Love from
-        Nagaland
+      <p>© {new Date().getFullYear()} C2 Tech. All rights reserved</p>
+      <p className=" font-mono text-sm">
+        Made with ❤️ from Nagaland |{' '}
+        <a href="mailto:ctwo.60two@gmail.com">ctwo.60two@gmail.com</a>
       </p>
     </footer>
   );
