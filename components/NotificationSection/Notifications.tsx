6b98c5d1b970d2cd3bb7480c07c389da135292f0
Captain Peanuts <talimayangamer@gmail.com>
2024-07-26 14:41:37 +0530

    notification url update 2

--- components/NotificationSection/Notifications.tsx
+++ components/NotificationSection/Notifications.tsx
@@ -2,11 +2,11 @@ export const notifications = [
   {
     text: '- First Announcement - IAAP Conference. ',
     linkText: 'Click to download Brochure.',
-    link: 'https://drive.google.com/file/d/1qbUoBk0dA-SDq5E3fAL58jMideaeVSYU/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/1IE1rqZxm_lDhEJFc0moIYJSPDx7yOzeH/view?usp=sharing',
   },
   {
     text: '- Details for the conference. ',
     linkText: 'Click to download Flyer.',
-    link: 'https://drive.google.com/file/d/1LmKvJk4n9NeA32aHWVgjoUuYCd9TW2en/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/1_FGgZxu25z8RTU7RTAAqLWoKLW_umW0c/view?usp=sharing',
   },
 ];
