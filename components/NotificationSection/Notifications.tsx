aaa5519965966f832e0e4164afbf98156daec1ba
Captain Peanuts <talimayangamer@gmail.com>
2024-09-19 02:59:32 +0530

    Notification update

--- components/NotificationSection/Notifications.tsx
+++ components/NotificationSection/Notifications.tsx
@@ -1,6 +1,6 @@
 export const notifications = [
   {
-    text: 'Third Announcement - IAAP Conference. ',
+    text: 'Fourth Announcement - IAAP Conference. ',
     linkText: 'Click to download Brochure.',
     link: 'https://drive.google.com/file/d/1B55sIV_7PdnluZdY9hD0Ay-3FevCYbLd/view?usp=sharing',
   },
