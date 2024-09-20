08fbde6a1c9692ab3c44f369163cb44b27d72831
Captain Peanuts <talimayangamer@gmail.com>
2024-09-20 13:41:42 +0530

    footer update

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="w-full py-4 bg-primary text-primary-foreground text-center self-end mt-5 container">
       <p>© {new Date().getFullYear()} C2 Tech. All rights reserved</p>
       <p className=" font-mono text-xs md:text-sm">
-        Made with ❤️ in Nagaland |{' '}
+        Made in Nagaland |{' '}
         <a href="mailto:ctwo.60two@gmail.com">ctwo.60two@gmail.com</a>
       </p>
     </footer>
