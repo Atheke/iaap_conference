c5683f568b386f1df1496bf62461d2d7f827d086
Atheke <jothharrison@gmail.com>
2025-03-11 15:55:27 +0530

    Added cre certificates

--- components/AchievementSection/Achievement.tsx
+++ components/AchievementSection/Achievement.tsx
@@ -3,6 +3,11 @@ export const achievements = [
     text: 'BEST ORAL POSTER PAPER ',
     linkText: 'Click to view',
     link: 'https://drive.google.com/file/d/170h2OQINY66CW1VsZ_eutkGsoGsHppl4/view?usp=sharing',
-  }  
-		
+  }, 
+	
+	{
+    text: 'CRE Participants Certificates',
+    linkText: 'Click to view',
+    link: 'https://drive.google.com/file/d/1qbHeGnK2YcvvYmrD8VocLlvOCL_Ef5Bo/view?usp=sharing',
+  }
 ];
